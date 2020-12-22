ac7edeffc25c5fdadf82c05d1908551362be705a
Philip <philip.nguyen@sjsu.edu>
2020-12-21 16:08:12 -0800

    Initial Commit

---
+++ Review/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Review
+//
+//  Created by Philip Nguyen on 12/21/20.
+//  Copyright © 2020 Philip Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
