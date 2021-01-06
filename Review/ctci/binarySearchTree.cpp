eb76517a0d9085e341ef01b112e2d119afdc9c2a
Philip <philip.nguyen@sjsu.edu>
2021-01-05 23:32:54 -0800

    started bst class

---
+++ Review/ctci/binarySearchTree.cpp
@@ -0,0 +1,9 @@
+//
+//  binarySearchTree.cpp
+//  Review
+//
+//  Created by Philip Nguyen on 1/5/21.
+//  Copyright © 2021 Philip Nguyen. All rights reserved.
+//
+
+#include "binarySearchTree.hpp"
---
+++ Review/ctci/binarySearchTree.hpp
@@ -0,0 +1,25 @@
+//
+//  binarySearchTree.hpp
+//  Review
+//
+//  Created by Philip Nguyen on 1/5/21.
+//  Copyright © 2021 Philip Nguyen. All rights reserved.
+//
+
+#ifndef binarySearchTree_hpp
+#define binarySearchTree_hpp
+
+#include <stdio.h>
+
+class TreeNode {
+    TreeNode* right;
+    TreeNode* left;
+    int data;
+};
+
+class BinarySearchTree {
+  
+    
+};
+
+#endif /* binarySearchTree_hpp */
