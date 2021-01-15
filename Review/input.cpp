7835ef9d213a1a59f4b18a86465eea65b6b69585
Philip <philip.nguyen@sjsu.edu>
2021-01-14 23:10:13 -0800

    comments for two sum

--- Review/input.cpp
+++ Review/input.cpp
@@ -98,6 +98,9 @@ std::string addStrings(std::string num1, std::string num2) {
 
 /**
  Two Sum
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
  */
 std::vector<int> twoSum(std::vector<int> & nums, int target) {
     // map is used to keep a set of factors
